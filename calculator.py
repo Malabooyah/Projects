0a8e4786d55b5f3ed5b11f363f9bd15c8c193cc0
Malabooyah <Malachi97Bennett@hotmail.com>
2025-11-29 18:43:10 -0700

    Update calculator.py
    -Validation for operation and number inputs
    -Added "%" function and "ANS" button feature

--- calculator.py
+++ calculator.py
@@ -1,8 +1,40 @@
-while True:
-	num1 = float(input("Please enter first number: "))
-	num2 = float(input("Please enter second number: "))
+#Prevents the program from breaking when incorrect number is entered
+def get_number(prompt):
+	global last_result
+	while True:
+		raw = input(prompt).strip().lower()
+		#Allows ANS(answer) if a previous result is present
+		if raw == "ans" :
+			if isinstance(last_result, (int, float)):
+				return last_result
+			else:
+				print("No previous answer saved yet!")
+				continue
 
-	op = input("Enter your operation (+,-,*,/,**): ").lstrip()
+		#Normal number handling
+		try:
+			return float(raw)
+		except ValueError:
+			print("Nahhh, that's not a number. Try again!")
+
+#Prevents the program from breaking when incorrect operation is entered
+def get_operation():
+	valid_ops = ["+", "-", "*", "/", "**", "%"]
+	while True:
+		op = input("Enter your operation (+,-,*,/,**,%): ").strip()
+		if op in valid_ops:
+			return op
+		else:
+			print("Thats not a valid operation...try again!")
+
+last_result = None
+
+#Calculator Loop
+while True:
+	num1 = get_number("Please enter first number: ")
+	num2 = get_number("Please enter second number: ")
+
+	op = get_operation()
 
 	if op == '+' :
 		result = num1 + num2
@@ -18,6 +50,12 @@ while True:
 			result = num1 / num2
 	elif op == '**' :
 		result = num1 ** num2
+	elif op == "%" :
+		if num2 == 0:
+			print("Can't do that in this one Buddy")
+			result = "undefined"
+		else:
+			result = num1 % num2
 	else:
 		print("Invalid operation!")
 		result = "undefined"
@@ -25,7 +63,12 @@ while True:
 
 	print(f"{num1} {op} {num2} = {result}")
 
+	# Updates last_result if the value of result is a number
+	if isinstance(result, (int, float)):
+		last_result = result
+
 	play_again = input("Do another calculation? (y/n): ").lower().strip()
 	if play_again != "y" :
 		print("Goodbye and Good Riddance!")
 		break
+
